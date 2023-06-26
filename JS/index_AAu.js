18c112c7c24021e8c8a8e5380e652b89918fff7d
RainbowYoshi <92651106+ArthurAugis@users.noreply.github.com>
2023-06-26 21:09:54 +0200

    Delete JS directory

--- JS/index_AAu.js
+++
@@ -1,8 +0,0 @@
-function functionMDP() {
-    var x = document.getElementById("MotDePasse");
-    if (x.type === "password") {
-        x.type = "text";
-    } else {
-        x.type = "password";
-    }
-}
